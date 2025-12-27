d249d1822eed60988801010fa6a75e3a597db88f
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-27 17:44:41 +0300

    first register user passed

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const { register } = require('./controller/auth')
+const connectDB = require('./config/db')
 
 app.use(express.json())
 
 app.post('/register', register)
 
-app.listen(process.env.PORT, async (req,res) => {
-    console.log(`server is running on the port ${process.env.PORT}`);
-})
+
+const startServer = async () => {
+    await connectDB()
+    app.listen(process.env.PORT, async (req,res) => {
+        console.log(`server is running on the port ${process.env.PORT}`);
+    })
+}
+
+startServer()
--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const mongoDB = async () => {
     const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`)
 
     if (connectionInstance) {
-        console.log(`Mongo Connected ${connectionInstance.env.host}`)
+        console.log(`Mongo Connected}`)
     }
 }
+
+module.exports = mongoDB
