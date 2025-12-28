0bbc8ffa1ca78d99347303145648456b5a396057
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-28 13:02:32 +0300

    separate the routes to the routes folder

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const { register } = require('./controller/auth')
+const  router  = require('./routes/authRoutes')
 const connectDB = require('./config/db')
 
 app.use(express.json())
-
-app.post('/register', register)
-
+app.use('/', router)
 
 const startServer = async () => {
     await connectDB()
-    app.listen(process.env.PORT, async (req,res) => {
+    app.listen(process.env.PORT, async (req, res) => {
         console.log(`server is running on the port ${process.env.PORT}`);
     })
 }
