8e54ec1a590c64cf9d096334729297c22ab9cef4
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-28 14:19:30 +0300

    login route

--- controller/auth.js
+++ controller/auth.js
@@ -9,4 +9,27 @@ const register = async (req, res) => {
 
 }
 
-module.exports = { register }
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'please enter all fields' })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' })
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+
+    if (!isPasswordCorrect) {
+        return res.status(401).json({ message: 'Invalid email or password' })
+    }
+
+    res.status(200).json({ message: 'Login Successful' })
+}
+
+
+module.exports = { register, login }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const { register } = require('../controller/auth')
+const { register, login } = require('../controller/auth')
 
-router.post('/register', register)
+router.post('/register', register).post('/login', login)
 
 module.exports = router
